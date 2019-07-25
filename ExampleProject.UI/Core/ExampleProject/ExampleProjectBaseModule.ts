077c9c5a9f4d37de0a111f1687d8200386e33dfb
Abdelrahman El-Barkouky <asbarkouky@gmail.com>
2019-07-25 19:25:30 +0200

    npm packages included

---
+++ ExampleProject.UI/Core/ExampleProject/ExampleProjectBaseModule.ts
@@ -0,0 +1,42 @@
+﻿import { ModuleWithProviders } from "@angular/compiler/src/core";
+import { NgModule } from "@angular/core";
+import { CodeShellModule } from "CodeShell";
+import { BrowserModule } from "@angular/platform-browser";
+import { ToastrModule } from "ngx-toastr";
+import { TranslateModule } from "@ngx-translate/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { Login } from "./Main/Login";
+import { AuthFilter } from "CodeShell/security";
+import { topBar } from "./Main/topBar";
+import { navigationSideBar } from "./Main/navigationSideBar";
+
+@NgModule({
+    declarations: [Login, topBar, navigationSideBar],
+
+    imports: [
+        CodeShellModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot()
+    ],
+    exports: [
+        CodeShellModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        ToastrModule,
+        TranslateModule,
+        Login,
+        topBar,
+        navigationSideBar
+    ]
+})
+export class ExampleProjectBaseModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: ExampleProjectBaseModule,
+            providers: [
+                AuthFilter,
+            ]
+        }
+    }
+}
