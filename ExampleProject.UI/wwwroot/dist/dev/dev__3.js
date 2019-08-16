4ae4ed91d4fea9f0c0cc1dd86d73539fd5a090a3
Abdelrahman El-Barkouky <asbarkouky@gmail.com>
2019-08-16 17:24:52 +0200

    moldster v2.1

---
+++ ExampleProject.UI/wwwroot/dist/dev/dev__3.js
@@ -0,0 +1,16 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
+
+/***/ "./MainApp/app/Auth/Users/Modals/ModalsModule.ts":
+/*!*******************************************************!*\
+  !*** ./MainApp/app/Auth/Users/Modals/ModalsModule.ts ***!
+  \*******************************************************/
+/*! exports provided: ModalsModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"ModalsModule\", function() { return ModalsModule; });\n/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ \"./node_modules/@angular/core/esm5/core.js\");\n/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ \"./node_modules/@angular/router/esm5/router.js\");\n/* harmony import */ var _Shared_SharedModule__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../Shared/SharedModule */ \"./MainApp/app/Shared/SharedModule.ts\");\n/* harmony import */ var codeshell_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! codeshell/core */ \"./node_modules/codeshell/core.js\");\n/* harmony import */ var _ngx_translate_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ngx-translate/core */ \"./node_modules/@ngx-translate/core/@ngx-translate/core.es5.js\");\nvar __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {\r\n    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;\r\n    if (typeof Reflect === \"object\" && typeof Reflect.decorate === \"function\") r = Reflect.decorate(decorators, target, key, desc);\r\n    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;\r\n    return c > 3 && r && Object.defineProperty(target, key, r), r;\r\n};\r\nvar __metadata = (undefined && undefined.__metadata) || function (k, v) {\r\n    if (typeof Reflect === \"object\" && typeof Reflect.metadata === \"function\") return Reflect.metadata(k, v);\r\n};\r\n\r\n\r\n\r\n\r\n\r\nvar ModalsModule = /** @class */ (function () {\r\n    function ModalsModule(trans, conf) {\r\n        trans.setDefaultLang(conf.Locale);\r\n        trans.use(conf.Locale);\r\n    }\r\n    ModalsModule = __decorate([\r\n        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__[\"NgModule\"])({\r\n            declarations: [],\r\n            exports: [],\r\n            imports: [\r\n                _Shared_SharedModule__WEBPACK_IMPORTED_MODULE_2__[\"SharedModule\"],\r\n                _angular_router__WEBPACK_IMPORTED_MODULE_1__[\"RouterModule\"].forChild([])\r\n            ]\r\n        }),\r\n        __metadata(\"design:paramtypes\", [_ngx_translate_core__WEBPACK_IMPORTED_MODULE_4__[\"TranslateService\"], codeshell_core__WEBPACK_IMPORTED_MODULE_3__[\"ServerConfigBase\"]])\r\n    ], ModalsModule);\r\n    return ModalsModule;\r\n}());\r\n\r\n\n\n//# sourceURL=webpack:///./MainApp/app/Auth/Users/Modals/ModalsModule.ts?");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=dev__3.js.map
--- VersionSetter.bat
+++ VersionSetter.bat
@@ -6,12 +6,12 @@ VersionSetter -p CodeShellCore.MQ 2.0.4
 VersionSetter -p CodeShellCore.Reporting 2.0.4
 VersionSetter -p CodeShellCore.Web 2.0.9
 
-VersionSetter -p CodeShellCore.Moldster 2.0.16
-VersionSetter -p CodeShellCore.Web.Razor 2.0.16
+VersionSetter -p CodeShellCore.Moldster 2.1.0
+VersionSetter -p CodeShellCore.Web.Razor 2.1.0
 
-VersionSetter -p Asga 2.0.4
-VersionSetter -p Asga.Common 2.0.4
-VersionSetter -p Asga.Auth 2.0.4
-VersionSetter -p Asga.Web 2.0.4
+VersionSetter -p Asga 2.0.9
+VersionSetter -p Asga.Common 2.0.9
+VersionSetter -p Asga.Auth 2.0.9
+VersionSetter -p Asga.Web 2.0.9
 
 PAUSE
