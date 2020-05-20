2cb2ccb6cfda6fb59239776895c2fe9e444b9e98
Abdelrahman El-Barkouky <asbarkouky@gmail.com>
2020-05-20 18:16:21 +0200

    rm some

--- ExampleProject.UI/Core/ExampleProject/Auth/Users/UserEditBase.ts
+++
@@ -1,9 +0,0 @@
-﻿import { EditComponentBase } from "codeshell/baseComponents";
-import { Injectable } from "@angular/core";
-import { UsersService } from "ExampleProject/Http";
-import { Shell } from "codeshell/core";
-
-@Injectable()
-export abstract class UserEditBase extends EditComponentBase{
-	get Service(): UsersService { return Shell.Injector.get(UsersService); }
-}
--- ExampleProject.UI/Core/ExampleProject/Auth/Users/UserListBase.ts
+++
@@ -1,9 +0,0 @@
-﻿import { ListComponentBase } from "codeshell/baseComponents";
-import { Injectable } from "@angular/core";
-import { UsersService } from "ExampleProject/Auth/Users/Http";
-import { Shell } from "codeshell/core";
-
-@Injectable()
-export abstract class UserListBase extends ListComponentBase{
-	get Service(): UsersService { return Shell.Injector.get(UsersService); }
-}
