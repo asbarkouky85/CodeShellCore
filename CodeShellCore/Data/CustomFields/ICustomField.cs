0790f48c920aefc47bd04470c6ed8a81bc98bf91
Abdelrahman El-Barkouky <asbarkouky@gmail.com>
2021-06-19 20:34:01 +0200

    init

--- CodeShellCore/Data/CustomFields/ICustomField.cs
+++ CodeShellCore/Data/CustomFields/ICustomField.cs
@@ -7,7 +7,10 @@ namespace CodeShellCore.Data.CustomFields
 {
   public interface ICustomField : IModel<long>, IEditable
     {
-         string EntityType { get; set; }         long EntityId { get; set; }         string Name { get; set; }         string Value { get; set; }
+         string EntityType { get; set; }
+         long EntityId { get; set; }
+         string Name { get; set; }
+         string Value { get; set; }
 
     }
 }
