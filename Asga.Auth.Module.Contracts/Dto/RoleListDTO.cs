c333f5e24cacee574bb89bda5edb4d3c60a9f426
Abdelrahman El-Barkouky <asbarkouky@gmail.com>
2021-07-24 08:36:33 +0200

    fix

--- Asga.Auth.Module.Contracts/Dto/RoleListDTO.cs
+++ Asga.Auth.Module.Contracts/Dto/RoleListDTO.cs
@@ -7,17 +7,17 @@ using System.Text;
 
 namespace Asga.Auth.Dto
 {
-    public class RoleListDTO : BaseDTO<long>, IDTO<Role>
+    public class RoleListDTO : BaseDTO<long>//, IDTO<Role>
     {
         public string Name { get; set; }
 
-        public static Expression<Func<Role, RoleListDTO>> Expression =>
-            d => new RoleListDTO
-            {
-                Id = d.Id,
-                Name = d.Name,
-                CreatedBy = d.CreatedBy,
-                CreatedOn = d.CreatedOn
-            };
+        //public static Expression<Func<Role, RoleListDTO>> Expression =>
+        //    d => new RoleListDTO
+        //    {
+        //        Id = d.Id,
+        //        Name = d.Name,
+        //        CreatedBy = d.CreatedBy,
+        //        CreatedOn = d.CreatedOn
+        //    };
     }
 }
